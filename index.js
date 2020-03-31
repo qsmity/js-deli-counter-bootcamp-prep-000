24082e930927746fe694b76c9e46c9f57feee715
alpha-acrobat-9971 <qsmity@gmail.com>
2020-03-31 10:14:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-// var takeNumber = function(currentLine, newName){
-//   var num = currentLine.length
-//   return `Welcome, ${newName}. You are the ${num + 1}th in line.`
-// }
+var takeNumber = function(currentLine, newName){
+  var num = currentLine.length
+  return `Welcome, ${newName}. You are the ${num + 1}th in line.`
+}
 
 // console.log(takeNumber(["ada", "zach", "corey"], "lesly"))
 
-// var nowServing = function(katzDeliLine){
-//   if(katzDeliLine.length > 0){
-//   const firstPerson = katzDeliLine[0]
-//   katzDeliLine.shift()
-//   return firstPerson
-//   } else {
-//     return "There is nobody waiting to be served!"
-//   }
-// }
+var nowServing = function(katzDeliLine){
+  if(katzDeliLine.length > 0){
+  const firstPerson = katzDeliLine[0]
+  katzDeliLine.shift()
+  return firstPerson
+  } else {
+    return "There is nobody waiting to be served!"
+  }
+}
 
 // console.log(nowServing([]))
 
@@ -29,4 +29,4 @@ var currentLine = function(currentLine){
   }
 }
 
-console.log(currentLine(["jada", "jason", "jayla"]))
+// console.log(currentLine(["jada", "jason", "jayla"]))
