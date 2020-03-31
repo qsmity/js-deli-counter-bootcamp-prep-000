d2c2279930a8ffc9d5b484db733db7ad18d8fb7f
alpha-acrobat-9971 <qsmity@gmail.com>
2020-03-31 09:33:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 var takeNumber = function(currentLine, newName){
-  
+  return `Welcome, ${name}. You are the ${currentLine}th in line.`
+}
+
+var nowServing = function(katzDeliLine){
+  if(katzDeliLine.length >= 1){
+  return katzDeliLine[0]
+  katzDeliLine.shift()
+  } else {
+    return "The line"
+  }
 }
