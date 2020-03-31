d1ef8ad83490bd648a7eef930e315d02fd359e2b
alpha-acrobat-9971 <qsmity@gmail.com>
2020-03-31 09:41:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 var takeNumber = function(currentLine, newName){
-  return `Welcome, ${name}. You are the ${currentLine}th in line.`
+  var num = currentLine.length
+  return `Welcome, ${name}. You are the ${num + 1}th in line.`
 }
 
-var nowServing = function(katzDeliLine){
-  if(katzDeliLine.length >= 1){
-  return katzDeliLine[0]
-  katzDeliLine.shift()
-  } else {
-    return "There is nobody waiting to be served!"
-  }
-}
+console.log(takeNumber(["ada", "zach", "corey"], "lesly"))
 
-var currentLine = function(currentLine){
-  if (currentLine > 0){
-  return `the line is currently:`
-  } else {
-    return 
-  }
-}
+// var nowServing = function(katzDeliLine){
+//   if(katzDeliLine.length >= 1){
+//   return katzDeliLine[0]
+//   katzDeliLine.shift()
+//   } else {
+//     return "There is nobody waiting to be served!"
+//   }
+// }
+
+// var currentLine = function(currentLine){
+//   if (currentLine > 0){
+//   return `the line is currently:`
+//   } else {
+//     return "The line is currently empty."
+//   }
+// }
